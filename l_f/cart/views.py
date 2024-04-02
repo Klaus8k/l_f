67c2c9d979912d60b758c5074356f004e37796ea
Klaus8k <klaus888@yandex.ru>
2024-04-02 18:42:44 +0300

    cart.view add

--- l_f/cart/views.py
+++ l_f/cart/views.py
@@ -6,6 +6,7 @@ from .forms import CartAddProductForm
 
 # Create your views here.
 
+
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
@@ -13,3 +14,9 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
+# https://pocoz.gitbooks.io/django-v-primerah/content/glava-7-sozdanie-internet-magazina/sozdanie-korzini/sozdanie-predstavlenii-korzini-pokupok/dobavlenie-elementov-v-korzinu.html
