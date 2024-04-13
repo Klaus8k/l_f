60d2784070179ed67c15dd8d4ba4bb2aaebd6387
Klaus8k <klaus888@yandex.ru>
2024-04-13 11:54:02 +0300

    test aadd

--- l_f/test.py
+++ l_f/test.py
@@ -1,10 +1,23 @@
-s = list(input())
-j = list(input())
+nickname = input()
+len_name = len(nickname) >= 8
+num_name = [i.isdigit() for i in nickname]
+alfa_name = [i.isalpha() for i in nickname]
+lower_name = [i.islower() for i in nickname]
+upper_name = [i.isupper() for i in nickname]
 
-count = 0
-for i in set(s):
-    if i in j:
-        count += j.count(i)
-        
-print(count)
-    
+
+def check_rules(data: list):
+    count = 0
+    for i in data:
+        if i == False:
+            count += 1
+    if len(data) == count:
+        return False
+    else:
+        return True
+
+
+rules_list = [[len_name,], num_name, alfa_name, lower_name, upper_name]
+result = [check_rules(i) for i in rules_list]
+
+print('YES' if all(result) else 'NO')
