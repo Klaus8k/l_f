1c748335ce78f0048ed236b4f79698d87059837e
Klaus8k <klaus888@yandex.ru>
2024-04-20 12:44:29 +0300

    1

--- l_f/test_2.py
+++ l_f/test_2.py
@@ -1,29 +1,60 @@
-from typing import *
+from bisect import bisect_left
+import string
 
+# first_dict = {'First':None}
+# second_dict = {'Second':None}
 
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        a = b = 0
-        ref = [nums[0],]
-        
-        for i in range(len(nums)):
-            if nums[a] in ref:
-                nums[a] = nums[a]+1
+first = []
+second = []
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    steps = []
+    counter = 1
+    for i in f.readlines():
+        i = i.strip()
+        if len(i) == 1:
+            hodov = i
+        else:
+            if counter % 2 == 0:
+                first.append([int(x) for x in i.split()])
             else:
-                
-                ref.append(nums[i])
-                a += 1
-        print(nums)
-
-        return b
-
-        # set_nums = set(nums)
-        # len_nums = len(nums)
-        # nums = sorted(list(set_nums))
-
-        # add_list = ['_' for i in range(len_nums - len(set_nums))]
-        # return len(nums), nums + add_list
+                second.append([int(x) for x in i.split()])
+        counter += 1
 
 
-nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-print(Solution().removeDuplicates(nums))
+def get_win(arr: list):
+    x = [i[0] for i in arr]
+    y = [i[1] for i in arr]
+    # go to x
+    for i in arr:
+        if x.count(i[0]) == 5: #got to all dotes where x.count == 5
+            repeat_dots = [item[1] for item in arr if item[0] == i[0]]
+            return max_long_sort(repeat_dots)
+        elif y.count(i[1]) == 5: #got to all dotes where x.count == 5
+            repeat_dots = [item[0] for item in arr if item[1] == i[1]]
+            print(repeat_dots)
+            return max_long_sort(repeat_dots)
+        elif 
+
+
+def max_long_sort(arr: list):
+    print(arr)
+    arr.sort()
+    cnt = 0
+    for i in range(1, len(arr)):
+        if arr[i] - arr[i-1] == 1:
+            cnt += 1
+            if cnt == 4:
+                return True
+        else: cnt = 0
+    return False
+
+    
+
+    #    [4, 1], [2, 1], [3, 1], [1, 1], [5, 1]
+    #    [[4, 1], [2, 1], [3, 1], [1, 1], [5, 1]]
+
+
+print(get_win([[4, 1], [2, 1], [3, 1], [1, 1], [5, 1]]))
+
+print(get_win([[4, 1], [2, 1], [3, 1], [1, 1], [5, 1]]))
