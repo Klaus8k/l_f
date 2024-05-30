805e13d044ceaa3eeee5e4a4f5e598738b550fa0
Klaus8k <klaus888@yandex.ru>
2024-05-30 17:52:27 +0300

    pass change add

--- l_f/account/urls.py
+++ l_f/account/urls.py
@@ -1,15 +1,26 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
+
 
 app_name = 'account'
 
 urlpatterns = [
     # path('login/', views.user_login, name='login')
-    
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
-    path('', views.dashboard, name='dashboard') # страница аккаунта в дашборде (то есть страница профиля)
-    
+
+    # url-адреса смены пароля
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(
+             success_url=reverse_lazy('account:password_change_done')),
+         name='password_change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
+    # страница аккаунта в дашборде (то есть страница профиля)
+    path('dashboard/', views.dashboard, name='dashboard')
+
 ]
