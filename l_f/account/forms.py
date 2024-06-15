c286c8bd3ce0f049432bc5d6623dfeb5c1c221ed
Klaus8k <klaus888@yandex.ru>
2024-06-15 13:40:20 +0300

    regRU

--- l_f/account/forms.py
+++ l_f/account/forms.py
@@ -1,15 +1,8 @@
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User
 from typing import Any
 from django import forms
-<<<<<<< HEAD
-from django.contrib.auth.models import User
-=======
-<<<<<<< HEAD
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.forms import PasswordChangeForm
-=======
-from django.contrib.auth.models import User
->>>>>>> c8716cc135391a253c88b23743226fe6076458fd
->>>>>>> 6f0f0587a916ca3ecf309a132c18cca079c5f0a9
 
 
 class LoginForm(forms.Form):
@@ -17,14 +10,12 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 class ChangePasswordForm_ru(PasswordChangeForm):
     pass
 
-=======
->>>>>>> 6f0f0587a916ca3ecf309a132c18cca079c5f0a9
+
+
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput)
@@ -40,7 +31,4 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
-<<<<<<< HEAD
-=======
->>>>>>> c8716cc135391a253c88b23743226fe6076458fd
->>>>>>> 6f0f0587a916ca3ecf309a132c18cca079c5f0a9
+
