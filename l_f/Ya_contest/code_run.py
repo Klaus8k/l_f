7d0189c6049cb26f1105d7eeea9da42c6ebcfe24
Oleg S home <tipkor@mail.ru>
2024-07-05 23:57:33 +0300

    abs mod num

--- l_f/Ya_contest/code_run.py
+++ l_f/Ya_contest/code_run.py
@@ -7,20 +7,32 @@ def main():
     # ask = int(input())
 
     len_arr = 5
-    arr = [-12, 3, 29, 1]
+    arr = [-12, 3, 29, -4]
     ask = -2
+
+    arr.sort()
+
+    x = None
+    y = None
+    
+    
 
     res = arr[0]
     mid = abs(ask)
-    
+
     if ask > 0:
         for i in arr:
             if abs(ask-i) < mid:
                 mid = abs(ask-i)
                 res = i
-                
-    elif ask < 0 or ask == 0:
-        res = min(arr)
+
+    elif ask < 0:
+        for i in arr:
+        
+            if abs(-i-ask) < mid:
+                    mid = abs(-i-ask)
+                    res = i
+
 
     print(res)
 
