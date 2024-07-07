debef5407e31634af6342101a5bc8702eb84cd9f
Oleg S home <tipkor@mail.ru>
2024-07-07 21:54:02 +0300

    код янд

--- l_f/Ya_contest/code_run.py
+++ l_f/Ya_contest/code_run.py
@@ -1,41 +1,25 @@
 import sys
 
-
+"""https://coderun.yandex.ru/selections/quickstart/problems/largest-product-three-numbers/description?currentPage=1&pageSize=20
+"""
 def main():
     # len_arr = int(input())
     # arr = [int(i) for i in input().split()]
-    # ask = int(input())
 
-    len_arr = 5
-    arr = [-12, 3, 29, -4]
-    ask = -2
-
-    arr.sort()
-
-    x = None
-    y = None
+    # len_arr = 9
+    arr = [-3, -5, -7, 0, -16]
+    arr_abs = [abs(i) for i in arr]
     
+    a = []
+    for i in range(3):
+        x = arr_abs.index(max(arr_abs))
+        arr_abs[x] = None
+        a.append(x)
+        # arr_abs.pop(x)
+        # arr_abs.pop(arr_abs.index(max(arr_abs)))
+        print(a)
+
+    print(arr[a[0]], arr[a[1]], arr[a[2]])
     
-
-    res = arr[0]
-    mid = abs(ask)
-
-    if ask > 0:
-        for i in arr:
-            if abs(ask-i) < mid:
-                mid = abs(ask-i)
-                res = i
-
-    elif ask < 0:
-        for i in arr:
-        
-            if abs(-i-ask) < mid:
-                    mid = abs(-i-ask)
-                    res = i
-
-
-    print(res)
-
-
 if __name__ == '__main__':
     main()
