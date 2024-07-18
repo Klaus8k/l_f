406400b4b1094bd7b894631a505fdfd29ff972f7
Klaus8k <klaus888@yandex.ru>
2024-07-18 18:28:38 +0300

    всплывающее окно

--- js/edu.js
+++ js/edu.js
@@ -1,10 +1,1 @@
-let button = document.getElementById('but');
-let x = document.getElementById('x');
-let y = document.getElementById('y');
-let res = document.getElementById('res');
-
-
-
-button.onclick = function() {
-    res.textContent = Number(x.value) + Number(y.value);
-}
+global
